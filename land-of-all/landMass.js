56a263f042c5de8979211c596e9450f6dcb7e8d4
David Omar Flores Chávez <david@davidomar.me>
2019-02-01 15:41:56 -0600

    Update propagation method to follow a certain angle

--- land-of-all/landMass.js
+++ land-of-all/landMass.js
@@ -1,12 +1,20 @@
+function getDisplacement(radius, angle) {
+  return {
+    x: radius * Math.cos(angle),
+    y: radius * Math.sin(angle)
+  }
+}
+
 function getPoints(center, radius, numberOfPoints) {
   let points = [];
 
   let step = (Math.PI * 2) / numberOfPoints;
 
   for (let i = 0; i < numberOfPoints; i++) {
+    let d = getDisplacement(radius, step * i)
     points.push({
-      x: center.x + radius * Math.cos(step * i),
-      y: center.y + radius * Math.sin(step * i)
+      x: center.x + d.x,
+      y: center.y + d.y
     });
   }
 
@@ -20,10 +28,12 @@ function LandMass(points) {
 
   this.update = function () {
     let newLandMass = []
+    let step = (Math.PI * 2) / this.points.length;
     for (let i = 0; i < points.length; i++) {
+      let d = getDisplacement(random(0, 2), step*i);
       newLandMass.push({
-        x: this.points[i].x + random(-1, 1),
-        y: this.points[i].y + random(-1, 1)
+        x: this.points[i].x + d.x,
+        y: this.points[i].y + d.y
       })
     }
     this.points = newLandMass
@@ -46,7 +56,6 @@ function LandMass(points) {
     for (var j = 0; j < this.history.length; j++) {
       let currentMass = this.history[j]
       stroke(200 - (j * 200 / this.history.length));
-      console.log(this.history.length)
       beginShape();
       for (var i = 0; i < currentMass.length; i++) {
         var pos = currentMass[i];
