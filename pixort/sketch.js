2df1f04067ec179f81ad2642e65f4d836f969d2d
David Omar Flores Chávez <david@davidomar.me>
2019-03-15 02:13:49 -0600

    Add a first version of pixel sorter
    For the moment it sorts with Hue, but this can be changed
    by changing the index for comparison in the function
    <Comparator> @l86. 1 will correspond to Saturation, and 2
    will correspond to Brightness.
    
    A good idea for further develpment includes finding a
    subset in <sortedPixels> with the same hue value, and then
    sort that subset using either Saturation, or Brithness, (and
    repeat if necessary).
    
    Also, it'd get better results if it sorts multiple Xs at a time,
    instead of only one.
    
    Try to implement a different drawing direction for the sorted
    pixel array.

--- pixort/sketch.js
+++ pixort/sketch.js
@@ -1,26 +1,90 @@
 var img;
 
 function preload() {
-  img = loadImage('ham.jpg');
-  // img = loadImage('http://www.petwebsite.com/hamsters/hamsters_images/syrian-hamster_000008437184.jpg');
+  // img = loadImage('ham.jpg');
+  img = loadImage('https://source.unsplash.com/random/720x360');
 }
 
 function setup() {
-  createCanvas(400, 400);
-  // img = createImage('http://www.petwebsite.com/hamsters/hamsters_images/syrian-hamster_000008437184.jpg');
-  // img.hide();
-  // background(100);
-  // loadImage('hamster.jpg', imageLoaded);
-  // image(img, 0, 0);  
+  createCanvas(720, 360);
+  pixelDensity(1);
   noLoop();
-
 }
 
 function draw() {
-  image(img, 0, 0, 400, 400);
-  // loadPixels();
+  image(img, 0, 0, img.width, img.height);
+
+  loadPixels();
+
+  let fourWidth = 4 * img.width;
+
+  for (let x = 0; x < img.height; x++) {
+
+    let sortedPixels = [];
+    colorMode(RGB)
+    for (let y = 0; y < fourWidth; y += 4) {
+      let loc = x * fourWidth + y;
+      let c = color(
+        pixels[loc],
+        pixels[loc + 1],
+        pixels[loc + 2]
+      )
+      sortedPixels.push([
+        hue(c),
+        saturation(c),
+        brightness(c)
+      ])
+    }
+
+    sortedPixels = sortedPixels.sort(Comparator)
+    colorMode(HSB);
+
+    let i = 0;
+
+    for (let sec_y = 0; sec_y < fourWidth; sec_y += 4) {
+      let loc = x * fourWidth + sec_y;
+      let c = sortedPixels[i]
+      let r = red(c);
+      let g = green(c);
+      let b = green(c);
+      // console.log(r, g, b)
+      pixels[loc] = r;
+      pixels[loc + 1] = g;
+      pixels[loc + 2] = b;
+      pixels[loc + 3] = 255;
+      i++;
+
+    }
+
+    updatePixels();
+  }
+
+  // sortedPixels = sortedPixels.sort(Comparator)
+  // colorMode(HSB);
+
+  // let i = 0;
+  // for (let x = 0; x < img.height; x++) {
+  //   for (let y = 0; y < fourWidth; y += 4) {
+  //     let loc = x * fourWidth + y;
+  //     let c = sortedPixels[i]
+  //     let r = red(c);
+  //     let g = green(c);
+  //     let b = green(c);
+  //     // console.log(r, g, b)
+  //     pixels[loc] = r;
+  //     pixels[loc + 1] = g;
+  //     pixels[loc + 2] = b;
+  //     pixels[loc + 3] = 255;
+  //     i++;
+  //   }
+  // }
+
+  // updatePixels();
+
 }
 
-// function imageLoaded(img) {
-//   image(img, 0, 0);
-// }
+function Comparator(a, b) {
+  if (a[0] < b[0]) return -1;
+  if (a[0] > b[0]) return 1;
+  return 0;
+}
