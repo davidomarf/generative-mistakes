880b847b5f6b58aa13c319f3c1f105dbc56486d6
David Omar Flores Chávez <david@davidomar.com>
2019-08-11 16:23:28 -0500

    Rename Trivoid and publish a first final version of it

--- trivoid/sketch.js
+++ trivoid/sketch.js
@@ -7,18 +7,21 @@ const CELLS_IN_MAP = 200;
 const BIG_CELL_SIZE = 400;
 
 const NOISE_SCALE = 0.012;
-const NOISE_SEED = 9423;
+// const NOISE_SEED = 9423;
+// const NOISE_SEED = 4450;
+const NOISE_SEED = Math.random() * 2000000;
 
 function setup() {
   createCanvas(WIDTH, HEIGHT);
   console.log("Seed: ", NOISE_SEED);
   noiseSeed(NOISE_SEED);
   noLoop();
+  background("#101520");
 }
 
 function isPointInSet(point, heights, threshold) {
   let cell = { i: floor(point.x / 10), j: floor(point.y / 10) };
-  return heights[cell.i][cell.j] > threshold;
+  return threshold(heights[cell.i][cell.j]);
 }
 
 function distanceBetweenPoints(a, b) {
@@ -35,7 +38,7 @@ function isTriangleTooBig(a, b, c, threshold) {
 
 function draw() {
   let heights = generateHeightMap();
-  createSpacesBetweenCells(heights);
+  // createSpacesBetweenCells(heights);
 
   let points = [];
 
@@ -46,7 +49,7 @@ function draw() {
           x: random(c_i * BIG_CELL_SIZE, (c_i + 1) * BIG_CELL_SIZE),
           y: random(c_j * BIG_CELL_SIZE, (c_j + 1) * BIG_CELL_SIZE)
         };
-        if (isPointInSet(point, heights, 0.5)) {
+        if (isPointInSet(point, heights, e => e > 0.5 && e < 0.7)) {
           points.push(point);
           // circle(point.x, point.y, 1);
         }
@@ -56,14 +59,18 @@ function draw() {
       // Get the Delaunay Triangulation for current Cell
       let delaunay = Delaunator.from(arr);
       push();
-      stroke(0);
+      colorMode(HSL);
+      noFill();
       strokeWeight(1);
       for (let del_i = 0; del_i < delaunay.triangles.length; del_i += 3) {
         let a = arr[delaunay.triangles[del_i]];
         let b = arr[delaunay.triangles[del_i + 1]];
         let c = arr[delaunay.triangles[del_i + 2]];
+        let height = heights[floor(a[0] / 10)][floor(a[1] / 10)];
 
-        if (isTriangleTooBig(a, b, c, 13)) {
+        let fillColor = color(20 + 40 * height * height, 50, 50);
+        stroke(fillColor);
+        if (isTriangleTooBig(a, b, c, 30 * height * height)) {
           continue;
         }
         beginShape();
@@ -78,50 +85,48 @@ function draw() {
     }
   }
 
-  // points = [];
-  // for (let i = 0; i < 15000; i++) {
-  //   let point = { x: random(WIDTH), y: random(HEIGHT) };
-  //   if (isPointInSet(point, heights, 0.49)) {
-  //     points.push();
-  //     circle(point.x, point.y, 1);
-  //   }
-  // }
-  // points = [];
-  // for (let i = 0; i < 15000; i++) {
-  //   let point = { x: random(WIDTH), y: random(HEIGHT) };
-  //   if (isPointInSet(point, heights, 0.48)) {
-  //     points.push();
-  //     circle(point.x, point.y, 1);
-  //   }
-  // }
-  // noStroke();
-  // fill(0);
-  // for (let h_i = 0; h_i < heights.length; h_i++) {
-  //   for (let h_j = 0; h_j < heights[h_i].length; h_j++) {
-  //     if (heights[h_i][h_j] > 0.5) {
-  //       circle(h_i * CELL_SIZE, h_j * CELL_SIZE, CELL_SIZE / 2);
-  //     }
-  //   }
-  // }
+  for (let c_i = 0; c_i < WIDTH / BIG_CELL_SIZE; c_i++) {
+    for (let c_j = 0; c_j < HEIGHT / BIG_CELL_SIZE; c_j++) {
+      for (let i = 0; i < 2000; i++) {
+        let point = {
+          x: random(c_i * BIG_CELL_SIZE, (c_i + 1) * BIG_CELL_SIZE),
+          y: random(c_j * BIG_CELL_SIZE, (c_j + 1) * BIG_CELL_SIZE)
+        };
+        if (isPointInSet(point, heights, e => e < 0.5 && e > 0.40)) {
+          points.push(point);
+          // circle(point.x, point.y, 1);
+        }
+      }
+      let arr = points.map(e => [e.x, e.y]);
 
-  // fill(100);
-  // for (let h_i = 0; h_i < heights.length; h_i++) {
-  //   for (let h_j = 0; h_j < heights[h_i].length; h_j++) {
-  //     if (heights[h_i][h_j] < 0.5 && heights[h_i][h_j] > 0.49) {
-  //       circle(h_i * CELL_SIZE, h_j * CELL_SIZE, CELL_SIZE / 2);
-  //     }
-  //   }
-  // }
+      // Get the Delaunay Triangulation for current Cell
+      let delaunay = Delaunator.from(arr);
+      push();
+      colorMode(HSL);
+      noFill();
+      strokeWeight(1);
+      for (let del_i = 0; del_i < delaunay.triangles.length; del_i += 3) {
+        let a = arr[delaunay.triangles[del_i]];
+        let b = arr[delaunay.triangles[del_i + 1]];
+        let c = arr[delaunay.triangles[del_i + 2]];
+        let height = heights[floor(a[0] / 10)][floor(a[1] / 10)];
 
-  // fill(200);
+        let fillColor = color(0, 50, 100 * height);
+        stroke(fillColor);
+        if (isTriangleTooBig(a, b, c, 20 - 30 * height * height)) {
+          continue;
+        }
+        beginShape();
+        vertex(a[0], a[1]);
+        vertex(b[0], b[1]);
+        vertex(c[0], c[1]);
+        endShape(CLOSE);
 
-  // for (let h_i = 0; h_i < heights.length; h_i++) {
-  //   for (let h_j = 0; h_j < heights[h_i].length; h_j++) {
-  //     if (heights[h_i][h_j] < 0.49 && heights[h_i][h_j] > 0.48) {
-  //       circle(h_i * CELL_SIZE, h_j * CELL_SIZE, CELL_SIZE / 2);
-  //     }
-  //   }
-  // }
+        points = [];
+      }
+      pop();
+    }
+  }
 }
 
 function createSpacesBetweenCells(heights) {
